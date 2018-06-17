97662d441a23d2328727a18d6ce0e27a260f05fd
gustavocsalvador <gustavocsalvador@gmail.com>
2018-06-17 19:52:19 -0300

    Add .gitignore, UNIX timestamp converter and data.txt

---
+++ weather/converter.js
@@ -0,0 +1,8 @@
+const convertUnixTimestamp = function(unix_timestamp) {
+    let date = new Date(unix_timestamp*1000);
+    let hours = date.getHours();
+    let minutes = "0" + date.getMinutes();
+    let seconds = "0" + date.getSeconds();
+    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+}
