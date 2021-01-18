b1ed5665a819dfae44dd544414333a71c7ed8b3e
g1stavo <gustavocsalvador@icloud.com>
2021-01-18 14:52:08 -0300

    chore: yarn securities update

--- weather/server.js
+++ weather/server.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var fetch = require("node-fetch");
-var app = express();
-
-var port = 8081;
-var router = express.Router();   
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-getData = async (url) => {
+const router = express.Router();
+
+const getData = async (url) => {
     const res = await fetch(url);
     const json = await res.json();
     return json;
-};
+}
 
 router.get('/', async (req, res) => {
-    const p = await getData('http://api.openweathermap.org/data/2.5/weather?id=6323121&units=metric&appid=997e9b9cb61c6f19903b67e67ff5e610');
+    const url = 'http://api.openweathermap.org/data/2.5/weather?id=6323121&units=metric&appid=997e9b9cb61c6f19903b67e67ff5e610';
+    const p = await getData(url);
     res.json(p);
 });
 
@@ -26,28 +26,25 @@ router.get('/convert', async (req, res) => {
     const time = req.query.unix_timestamp;
     let date = new Date(time * 1000);
     let hours = date.getHours();
-    let minutes = "0" + date.getMinutes();
-    let seconds = "0" + date.getSeconds();
-    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    let minutes = `0 ${date.getMinutes()}`;
+    let seconds = `0 ${date.getSeconds()}`;
+    let formattedTime = `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
     res.json({'time': formattedTime});
 });
 
 app.use('/api', router);
 
-init = () =>{
+const init = (port = 8081) => {
     app = app.listen(port);
-};
+}
 
-stop = () => {
+const stop = () => {
     app.close();
-};
+}
 
 if (process.argv.indexOf('start') != -1) {
     init();
-    console.log("Server listening on port: ", port);
+    console.log(`Server listening on port: ${port}`);
 };
 
-module.exports = {
-    init,
-    stop
-};
+module.exports = { init, stop };
